06bf8080bbd3ff5210f8dc54f31a034137a3e12e
nazart jara huaman <nazarjb@hotmail.com>
2012-11-03 16:23:30 -0500

    cambios nazart

---
+++ source/application/modules/default/controllers/NosotrosController.php
@@ -0,0 +1,18 @@
+<?php
+
+class Default_NosotrosController extends Core_Controller_Action       
+{
+    public function init() {
+        parent::init();
+    }
+    public function indexAction()
+    {           
+        
+    } 
+    public function visionMisionAction()
+    {           
+        
+    } 
+    
+}
+
