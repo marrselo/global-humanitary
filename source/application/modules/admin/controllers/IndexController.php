825021d300bb90d6c65fbd24ede0abcf3820750f
marrselo <marrselo@gmailcom>
2012-11-17 00:46:49 -0500

    marrselO

--- source/application/modules/admin/controllers/IndexController.php
+++ source/application/modules/admin/controllers/IndexController.php
@@ -6,14 +6,8 @@ class Admin_IndexController extends Core_Controller_ActionAdmin
         parent::init();
     }
     public function indexAction()
-    {           
-        
+    {                   
         $this->view->banner = Application_Entity_Queries::getBanner();               
-<<<<<<< HEAD
-         
-         
-=======
->>>>>>> fd4d8d8f3a787879364ca0ed18b237db1d6f43ac
     } 
     public function deleteAction()
     {
