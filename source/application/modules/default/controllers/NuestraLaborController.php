c2b7866e65777046f278f6d866473ccf1f9f18e7
marrselo <marrselo@gmailcom>
2012-12-09 15:57:25 -0500

    marrselo

---
+++ source/application/modules/default/controllers/NuestraLaborController.php
@@ -0,0 +1,47 @@
+<?php
+
+class Default_NuestraLaborController extends Core_Controller_ActionDefault {
+
+    public function init() {
+        parent::init();
+        $this->view->navigationLeft = new Zend_Navigation($this->getNavLeft());
+    }
+
+    public function indexAction() {
+        
+    }
+    public function imagenesAction() {
+        $this->view->imagenes = Application_Entity_Queries::listingImagenes();
+    }
+    public function videosAction() {
+        $this->view->videos = Application_Entity_Queries::listingVideos();
+    }
+    public function fondosEscritorioAction() {
+        $this->view->wallpaper = Application_Entity_Queries::listingWallpaper();
+    }
+
+    public function getNavLeft() {
+        return array(
+            array(
+                'label' => 'Imágenes',
+                'module' => 'default',
+                'controller' => 'descarga',
+                'action' => 'imagenes',
+            ),
+            array(
+                'label' => 'Videos',
+                'module' => 'default',
+                'controller' => 'descarga',
+                'action' => 'videos',
+            ),
+            array(
+                'label' => 'Fondos de Escritorio',
+                'module' => 'default',
+                'controller' => 'descarga',
+                'action' => 'fondos-escritorio',
+            )
+        );
+    }
+
+}
+
